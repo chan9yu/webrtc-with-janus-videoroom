565e1de84a16f16c6e7211c72c68d405b3ec6524
chan9yu <wry5533@gmail.com>
2024-06-13 20:57:12 +0900

    feat: server 코드 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,28 @@
 import { AppService } from '../services/AppService';
 
-new AppService();
+const appsService = new AppService();
 
 export default function HomePage() {
+	const handleConnectJanus = () => {
+		appsService.connectJanus();
+	};
+
+	const handleCreateRoom = () => {
+		appsService.createRoom();
+	};
+
+	const handleJoinRoom = () => {
+		appsService.joinRoom('옥분이');
+	};
+
 	return (
 		<div className="container">
-			<h1>Janus 1:n Video Room</h1>
-			<video id="localVideo" autoPlay muted></video>
-			<div id="remoteVideos"></div>
+			<h1>Janus 1:n Video Room TEST</h1>
+			<button onClick={handleConnectJanus}>1. connectJanus</button>
+			<br />
+			<button onClick={handleCreateRoom}>3. create Room</button>
+			&nbsp;
+			<button onClick={handleJoinRoom}>4. join Room</button>
 		</div>
 	);
 }
