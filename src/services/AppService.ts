890f2e40d12c05e666536ffa68b5fd84328bcd76
chan9yu <wry5533@gmail.com>
2024-06-13 20:56:48 +0900

    refactor: 서비스 로직 추가 및 수정

--- src/services/AppService.ts
+++ src/services/AppService.ts
@@ -1,215 +1,145 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-import { RTCManager } from '../modules/webrtc/RTCManager';
+import * as janusMessage from '../modules/janus/janusMessages';
 import { SocketManager, WEB_SOCKET_EVENTS } from '../modules/socket/SocketManager';
+import { RTCManager, SESSION_TYPE } from '../modules/webrtc/RTCManager';
+
+enum JanusTransactionState {
+	CREATE_SESSION = 'CREATE_SESSION',
+	PLUGIN_ATTACH = 'PLUGIN_ATTACH',
+	CREATE_ROOM = 'CREATE_ROOM',
+	PUBLISHER_JOIN_ROOM = 'PUBLISHER_JOIN_ROOM',
+	SUBSCRIBER_JOIN_ROOM = 'SUBSCRIBER_JOIN_ROOM',
+
+	TERMINATE_ROOM = 'TERMINATE_ROOM',
+	SEND_OFFER_SDP = 'SEND_OFFER_SDP',
+	SEND_ANSWER_SDP = 'SEND_ANSWER_SDP',
+	SEND_CANDIDATE = 'SEND_CANDIDATE',
+	KEEP_ALIVE = 'KEEP_ALIVE',
+	TERMINATE_SESSION = 'TERMINATE_SESSION'
+}
 
 export class AppService {
-	private server: string;
-	private socketManager: SocketManager;
-	private rtcManager: RTCManager;
-	private sessionId: number | null;
-	private handleId: number | null;
+	private sessionId: string | null;
+	private handleId: string | null;
 	private roomId: number;
+	private isRoomCreator = false;
+	private rtcManager: RTCManager;
+	private transactionState: JanusTransactionState;
+	private janusSocketManager: SocketManager;
 
 	constructor() {
-		this.server = 'wss://janus.conf.meetecho.com/ws';
 		this.sessionId = null;
 		this.handleId = null;
-		this.roomId = 1234; // 원하는 방 ID 설정
-		this.rtcManager = new RTCManager();
-		this.socketManager = new SocketManager(this.server, 'janus-protocol');
-		this.initWebSocketEvents();
+		this.roomId = 1234;
+		this.rtcManager = RTCManager.getInstance();
+	}
+
+	private async createOfferSDP() {
+		if (!this.sessionId) throw new Error('');
+		const peerId = this.rtcManager.createPeerId(this.sessionId, SESSION_TYPE.VIDEO);
+		this.rtcManager.getPeer(peerId) || this.rtcManager.createPeer(peerId);
+		const offer = await this.rtcManager.createOfferSDP(peerId);
+		return offer.sdp;
+	}
+
+	private onJanusMessage(msg: MessageEvent) {
+		const message = JSON.parse(msg.data);
+
+		switch (this.transactionState) {
+			case JanusTransactionState.CREATE_SESSION:
+				if (message.janus === 'success') {
+					this.sessionId = message.data.id;
+					this.attachPlugin();
+				}
+				break;
+			case JanusTransactionState.PLUGIN_ATTACH:
+				if (message.janus === 'success') {
+					this.handleId = message.data.id;
+				}
+				break;
+			case JanusTransactionState.CREATE_ROOM:
+				if (message.janus === 'success') {
+					console.log('### CREATE_ROOM: ', message);
+				}
+				break;
+			case JanusTransactionState.PUBLISHER_JOIN_ROOM:
+				if (message.janus === 'event') {
+					console.log('### PUBLISHER_JOIN_ROOM: ', message);
+				}
+				break;
+			case JanusTransactionState.SUBSCRIBER_JOIN_ROOM:
+				if (message.janus === 'event') {
+					console.log('### SUBSCRIBER_JOIN_ROOM: ', message);
+					this.createOfferSDP().then(sdp => sdp && this.sendConfigureMessage(sdp));
+				}
+				break;
+		}
 	}
 
 	private initWebSocketEvents() {
-		this.socketManager.addListener(WEB_SOCKET_EVENTS.ON_OPEN, () => {
-			console.log('JanusClient::socket event on open');
-			this.createSession();
+		this.janusSocketManager.addListener(WEB_SOCKET_EVENTS.ON_OPEN, () => {
+			console.log('JanusService::socket event on open');
+			this.transactionState = JanusTransactionState.CREATE_SESSION;
+			const message = janusMessage.makeCreateSessionMessage();
+			this.janusSocketManager.send(message);
 		});
 
-		this.socketManager.addListener(WEB_SOCKET_EVENTS.ON_MESSAGE, (msg: MessageEvent<any>) => {
-			const data = JSON.parse(msg.data);
-			console.log('JanusClient::socket event on message', data);
-			this.handleMessage(data);
+		this.janusSocketManager.addListener(WEB_SOCKET_EVENTS.ON_MESSAGE, (msg: MessageEvent) => {
+			console.log('JanusService::socket event on message');
+			this.onJanusMessage(msg);
 		});
 
-		this.socketManager.addListener(WEB_SOCKET_EVENTS.ON_ERROR, (error: Event) => {
-			console.log('JanusClient::socket event on error', error);
+		this.janusSocketManager.addListener(WEB_SOCKET_EVENTS.ON_ERROR, (error: Event) => {
+			console.log('JanusService::socket event on error', error);
 		});
 
-		this.socketManager.addListener(WEB_SOCKET_EVENTS.ON_CLOSE, () => {
-			console.log('JanusClient::socket event on close');
+		this.janusSocketManager.addListener(WEB_SOCKET_EVENTS.ON_CLOSE, () => {
+			console.log('JanusService::socket event on close');
 		});
 	}
 
-	private createSession() {
-		const message = {
-			janus: 'create',
-			transaction: this.randomString(12)
-		};
-		this.socketManager.send(message);
+	public connectJanus() {
+		console.log('JanusService::connectJanus');
+		const url = `wss://janus.conf.meetecho.com/ws`;
+		this.janusSocketManager = new SocketManager(url, 'janus-protocol');
+		this.initWebSocketEvents();
 	}
 
-	private attachPlugin() {
-		const message = {
-			janus: 'attach',
-			plugin: 'janus.plugin.videoroom',
-			transaction: this.randomString(12),
-			session_id: this.sessionId
-		};
-		this.socketManager.send(message);
+	public attachPlugin() {
+		if (!this.sessionId) throw new Error('');
+		this.transactionState = JanusTransactionState.PLUGIN_ATTACH;
+		const message = janusMessage.makeAttachPluginMessage(this.sessionId);
+		this.janusSocketManager.send(message);
 	}
 
-	private joinRoom() {
-		const message = {
-			janus: 'message',
-			body: {
-				request: 'join',
-				room: this.roomId,
-				ptype: 'publisher',
-				display: 'user' + Math.floor(Math.random() * 1000)
-			},
-			transaction: this.randomString(12),
-			session_id: this.sessionId,
-			handle_id: this.handleId
-		};
-		this.socketManager.send(message);
+	public createRoom() {
+		if (!this.sessionId || !this.handleId) throw new Error('');
+		this.isRoomCreator = true;
+		this.transactionState = JanusTransactionState.CREATE_ROOM;
+		const message = janusMessage.makeCreateRoomMessage(this.sessionId, this.handleId, { room: this.roomId });
+		this.janusSocketManager.send(message);
 	}
 
-	private createOffer(stream: MediaStream) {
-		const pc = this.rtcManager.createPeerConnection(
-			'local',
-			stream,
-			event => {
-				console.log('Remote track received:', event);
-				this.handleRemoteStream(event.streams[0], 'remote');
-			},
-			candidate => {
-				const message = {
-					janus: 'trickle',
-					candidate: candidate,
-					transaction: this.randomString(12),
-					session_id: this.sessionId,
-					handle_id: this.handleId
-				};
-				this.socketManager.send(message);
-			}
+	public joinRoom(userId: string) {
+		if (!this.sessionId || !this.handleId) throw new Error('');
+		this.transactionState = this.isRoomCreator
+			? JanusTransactionState.PUBLISHER_JOIN_ROOM
+			: JanusTransactionState.SUBSCRIBER_JOIN_ROOM;
+		const message = janusMessage.makeJoinRoomMessage(this.sessionId, this.handleId, {
+			room: this.roomId,
+			displayName: userId
+		});
+		this.janusSocketManager.send(message);
+	}
+
+	public sendConfigureMessage(sdp: string) {
+		if (!this.sessionId || !this.handleId) throw new Error('');
+		this.transactionState = JanusTransactionState.SEND_OFFER_SDP;
+		const message = janusMessage.makeConfigureMessage(
+			this.sessionId,
+			this.handleId,
+			{ audio: true, video: true },
+			{ sdp }
 		);
-
-		pc.createOffer().then(offer => {
-			pc.setLocalDescription(offer);
-			const message = {
-				janus: 'message',
-				body: { request: 'publish', audio: true, video: true },
-				jsep: offer,
-				transaction: this.randomString(12),
-				session_id: this.sessionId,
-				handle_id: this.handleId
-			};
-			this.socketManager.send(message);
-		});
-
-		return pc;
-	}
-
-	private handleRemoteStream(stream: MediaStream, id: string) {
-		const videoElement = document.createElement('video');
-		videoElement.id = `remoteVideo-${id}`;
-		videoElement.srcObject = stream;
-		videoElement.autoplay = true;
-		videoElement.playsInline = true;
-		document.getElementById('remoteVideos')?.appendChild(videoElement);
-	}
-
-	private handleMessage(data: any) {
-		console.log('Received message:', data);
-
-		if (data.janus === 'success') {
-			if (data.transaction) {
-				if (!this.sessionId) {
-					this.sessionId = data.data.id;
-					this.attachPlugin();
-				} else if (!this.handleId) {
-					this.handleId = data.data.id;
-					this.joinRoom();
-				}
-			}
-		} else if (data.janus === 'event' && data.plugindata) {
-			const event = data.plugindata.data.videoroom;
-
-			if (event === 'joined') {
-				navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
-					this.createOffer(stream);
-					const localVideo = document.getElementById('localVideo') as HTMLVideoElement;
-					localVideo.srcObject = stream;
-				});
-			} else if (event === 'event' && data.plugindata.data.publishers) {
-				data.plugindata.data.publishers.forEach((publisher: any) => {
-					this.subscribeToPublisher(publisher.id);
-				});
-			}
-		} else if (data.janus === 'event' && data.jsep) {
-			if (data.jsep.type === 'offer') {
-				const pc = this.rtcManager.createPeerConnection(
-					data.sender,
-					new MediaStream(),
-					event => {
-						console.log('Remote track received:', event);
-						this.handleRemoteStream(event.streams[0], data.sender);
-					},
-					candidate => {
-						const message = {
-							janus: 'trickle',
-							candidate: candidate,
-							transaction: this.randomString(12),
-							session_id: this.sessionId,
-							handle_id: this.handleId
-						};
-						this.socketManager.send(message);
-					}
-				);
-
-				pc.setRemoteDescription(new RTCSessionDescription(data.jsep))
-					.then(() => pc.createAnswer())
-					.then(answer => pc.setLocalDescription(answer))
-					.then(() => {
-						const message = {
-							janus: 'message',
-							body: { request: 'start', room: this.roomId },
-							jsep: pc.localDescription,
-							transaction: this.randomString(12),
-							session_id: this.sessionId,
-							handle_id: this.handleId
-						};
-						this.socketManager.send(message);
-					});
-			}
-		}
-	}
-
-	private subscribeToPublisher(publisherId: string) {
-		const message = {
-			janus: 'message',
-			body: {
-				request: 'join',
-				room: this.roomId,
-				ptype: 'subscriber',
-				feed: publisherId
-			},
-			transaction: this.randomString(12),
-			session_id: this.sessionId,
-			handle_id: this.handleId
-		};
-		this.socketManager.send(message);
-	}
-
-	private randomString(len: number) {
-		const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-		let randomString = '';
-		for (let i = 0; i < len; i++) {
-			const randomPoz = Math.floor(Math.random() * charSet.length);
-			randomString += charSet.substring(randomPoz, randomPoz + 1);
-		}
-		return randomString;
+		this.janusSocketManager.send(message);
 	}
 }
