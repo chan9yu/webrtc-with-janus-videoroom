73aa167f225e7ed128761aa18d367583466ba314
chan9yu <wry5533@gmail.com>
2024-06-14 15:51:14 +0900

    refactor: rtc manager 코드 변경

--- src/modules/webrtc/RTCManager.ts
+++ src/modules/webrtc/RTCManager.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from 'events';
-import { SESSION_TYPE } from './constants';
+
+export type TrackKind = 'video' | 'audio' | 'both';
 
 export type ICEServerInfo = {
 	urls: string;
@@ -8,23 +9,22 @@ export type ICEServerInfo = {
 };
 
 export class RTCManager extends EventEmitter {
-	private static instance?: RTCManager;
+	private peer: RTCPeerConnection | null = null;
+	private sourceStream?: MediaStream;
 
-	private peerList: Map<string, RTCPeerConnection> = new Map();
-	private iceServers: ICEServerInfo[] = [];
+	public readonly peerId: string;
 
-	private constructor() {
+	constructor(peerId: string, options?: RTCConfiguration) {
 		super();
+		this.peer = new RTCPeerConnection(options);
+		this.peerId = peerId;
+		this.initPeerEvents();
 	}
 
-	public static getInstance() {
-		if (!RTCManager.instance) {
-			RTCManager.instance = new RTCManager();
-		}
-		return RTCManager.instance;
-	}
+	private initPeerEvents() {
+		const peer = this.peer;
+		if (!peer) throw new Error('peer not found');
 
-	private initPeerEvents(id: string, peer: RTCPeerConnection) {
 		peer.onconnectionstatechange = event => {
 			console.log(event);
 		};
@@ -50,52 +50,100 @@ export class RTCManager extends EventEmitter {
 		};
 	}
 
-	public createPeerId(id: string, sessionType: SESSION_TYPE) {
-		const peerId = `${id}-${sessionType}`;
-		return peerId;
-	}
-
-	public createPeer(peerId: string, options?: RTCConfiguration) {
-		const configuration = { ...options, iceServers: this.iceServers };
-		const peer = new RTCPeerConnection(configuration);
-		this.initPeerEvents(peerId, peer);
-		this.peerList.set(peerId, peer);
-	}
-
-	public removePeer(peerId: string) {
-		this.peerList.get(peerId)?.close();
-		this.peerList.delete(peerId);
-	}
-
-	public resetPeer(peerId: string, options?: RTCConfiguration) {
-		this.removePeer(peerId);
-		this.createPeer(peerId, options);
-	}
-
-	public getPeer(peerId: string) {
-		const peer = this.peerList.get(peerId);
+	private getPeer() {
+		const peer = this.peer;
+		if (!peer) throw new Error('peer not found');
 		return peer;
 	}
 
-	public addTrack(peerId: string, stream: MediaStream) {
-		const peer = this.getPeer(peerId);
-		if (!peer) throw new Error('peer not found');
+	private replaceSenderTrack(stream: MediaStream, kind: TrackKind) {
+		const peer = this.getPeer();
+		const track = kind === 'video' ? this.sourceStream?.getVideoTracks()[0] : stream?.getAudioTracks()[0];
+
+		if (track) {
+			peer.getSenders().forEach(sender => {
+				if (sender.track?.kind === kind) {
+					sender.track.stop();
+					sender.replaceTrack(track);
+				}
+			});
+		}
+
+		if (kind === 'both') {
+			this.replaceSenderTrack(stream, 'video');
+		}
+	}
+
+	public addTrack(stream: MediaStream) {
+		const peer = this.getPeer();
+		const senders = peer.getSenders();
+		const sended = senders.length > 0 && senders[0].track;
+
+		if (sended) {
+			senders.forEach(sender => {
+				if (sender.track) {
+					this.replaceTrack(stream, (sender.track.kind as TrackKind) || 'both');
+				}
+			});
+		} else {
+			this.sourceStream = stream;
+			const cloneStream = this.sourceStream.clone();
+			cloneStream.getTracks().forEach(track => {
+				peer.addTrack(track, stream);
+			});
+		}
+	}
+
+	public replaceTrack(stream: MediaStream, kind: TrackKind) {
+		this.sourceStream?.getTracks().forEach(track => {
+			if (kind === 'both' || track.kind === kind) {
+				track.stop();
+				this.sourceStream?.removeTrack(track);
+			}
+		});
+
 		stream.getTracks().forEach(track => {
-			peer.addTrack(track, stream);
+			if (kind === 'both' || track.kind === kind) {
+				this.sourceStream?.addTrack(track);
+			}
+		});
+
+		this.replaceSenderTrack(stream, kind);
+	}
+
+	public removeTrack(kind = 'both') {
+		if (this.sourceStream) {
+			this.sourceStream.getTracks().forEach(track => {
+				if (kind === 'both' || track.kind === kind) {
+					track.stop();
+				}
+			});
+
+			if (this.sourceStream.getTracks().length <= 0) {
+				delete this.sourceStream;
+			}
+		}
+	}
+
+	public muteStream(id: string, kind: 'audio' | 'video', mute = true) {
+		const peer = this.getPeer();
+		const senders = peer.getSenders();
+		senders.forEach(sender => {
+			if (sender.track?.kind === kind) {
+				sender.track.enabled = !mute;
+			}
 		});
 	}
 
-	public async createOfferSDP(peerId: string, options?: RTCOfferOptions) {
-		const peer = this.getPeer(peerId);
-		if (!peer) throw new Error('peer not found');
+	public async createOfferSDP(options?: RTCOfferOptions) {
+		const peer = this.getPeer();
 		const offer = await peer.createOffer(options);
 		peer.setLocalDescription(offer);
 		return offer;
 	}
 
-	public async setRemoteSDP(peerId: string, sdp: RTCSessionDescriptionInit) {
-		const peer = this.getPeer(peerId);
-		if (!peer) throw new Error('peer not found');
+	public async setRemoteSDP(sdp: RTCSessionDescriptionInit) {
+		const peer = this.getPeer();
 		await peer.setRemoteDescription(sdp);
 		if (sdp.type === 'offer') {
 			const answer = await peer.createAnswer();
@@ -105,42 +153,26 @@ export class RTCManager extends EventEmitter {
 		return null;
 	}
 
-	public async addIceCandidate(peerId: string, candidate: RTCIceCandidateInit) {
-		const peer = this.getPeer(peerId);
-		if (!peer) throw new Error('peer not found');
+	public setRemoteIceCandidate(candidate: RTCIceCandidateInit) {
+		const peer = this.getPeer();
 		if (!peer.localDescription) throw new Error(`peer local description not found`);
-		await peer.addIceCandidate(candidate);
-	}
 
-	public setRemoteIceCandidate(peerId: string, candidate: RTCIceCandidateInit) {
 		let tryCount = 0;
 		const addIceCandidate = async () => {
 			try {
-				await this.addIceCandidate(peerId, candidate);
+				await peer.addIceCandidate(candidate);
 			} catch (error) {
 				setTimeout(() => {
 					++tryCount < 10 ? addIceCandidate() : console.error('invalid peer state');
 				}, 1000);
 			}
 		};
+
 		addIceCandidate();
 	}
 
-	public muteStream(id: string, kind: 'audio' | 'video', mute = true) {
-		const peer = this.getPeer(id);
-		if (!peer) throw new Error('peer not found');
-		const senders = peer.getSenders();
-		senders.forEach(sender => {
-			if (sender.track?.kind === kind) {
-				sender.track.enabled = !mute;
-			}
-		});
-	}
-
 	public clear() {
-		while (this.peerList.size > 0) {
-			this.removePeer(Array.from(this.peerList.keys())[0]);
-		}
-		this.peerList.clear();
+		this.peer?.close();
+		this.peer = null;
 	}
 }
